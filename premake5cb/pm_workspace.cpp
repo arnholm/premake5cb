51efca9c2de580cb73d8ea9784c510cac8b2d556
Carsten Arnholm <arnholm@arnholm.org>
2021-07-28 16:52:30 +0200

    Added feature to allow including external dependency file if it exists

--- premake5cb/pm_workspace.cpp
+++ premake5cb/pm_workspace.cpp
@@ -39,6 +39,21 @@ void pm_workspace::premake_export(std::ostream& out)
    out << "   return \"build/\".._ACTION" << std::endl;
    out << "end" << std::endl;
 
+   out << std::endl;
+   out << "function file_exists(name)" << std::endl;
+   out << "   local f=io.open(name,\"r\")" << std::endl;
+   out << "   if f~=nil then io.close(f) return true else return false end" << std::endl;
+   out << "end" << std::endl;
+
+   const std::string external_dependencies = name().ToStdString() + "_external_dependencies.lua";
+   out << std::endl;
+   out << "function external_dependencies()" << std::endl;
+   out << "    if file_exists(\""<<external_dependencies<<"\") then" << std::endl;
+   out << "       include \""<<external_dependencies<<"\"" << std::endl;
+   out << "       print (\"including '"<<external_dependencies<<"'\")" << std::endl;
+   out << "    end" << std::endl;
+   out << "end" << std::endl;
+
    if(post_build_copy) {
       out << std::endl;
       out << "function post_build_copy()" << std::endl;
@@ -50,6 +65,7 @@ void pm_workspace::premake_export(std::ostream& out)
    out << std::endl;
    out << "workspace \"" << name() << "\"" << std::endl;
    out << "\tlocation ( " << location_path() <<" )"<< std::endl;
+   out << "\texternal_dependencies()"<< std::endl;
    if(post_build_copy) out << "\tpost_build_copy()"<< std::endl;
 
    // export the workspace settings
